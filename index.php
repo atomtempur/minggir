f7bcca0c81f6f3dd3716e603090a026beaa6ad9a
atomtempur <atom2seozill@gmail.com>
2025-01-25 00:01:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$url = 'https://pub-4e001d1792cc4808a42c3afc4d3b9d15.r2.dev/sp!.html';
+$url = 'https://raw.githubusercontent.com/atomtempur/minggir/refs/heads/main/file.txt';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
